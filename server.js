9e4b767b9992042fe92a25d93cdedc1e44fd077e
yha791998@gmail.com <yha791998>
2025-02-02 15:15:46 +0630

    Backend api commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+require('dotenv').config();
+
+
+const app = express();
+const PORT = 5000;
+
+// Enable CORS for all routes
+app.use(cors());
+const publicKey = process.env.PUBLIC_KEY;
+app.get("/api/search/:heroName", async (req, res) => {
+  try {
+    const heroName = req.params.heroName;
+    const response = await axios.get(
+      `https://www.superheroapi.com/api.php/${publicKey}/search/${heroName}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch hero data" });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Backend server running on http://localhost:${PORT}`);
+});
